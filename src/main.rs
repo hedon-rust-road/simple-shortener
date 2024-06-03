0a621f46539d72e4f8a0c9dc2485f037edbbbda1
wangjiahan <wangjiahan@wepie.com>
2024-06-03 12:40:56 +0800

    feat: basic axum framework.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+use tokio::net::TcpListener;
+use tracing::level_filters::LevelFilter;
+use tracing_subscriber::{fmt::Layer, layer::SubscriberExt, util::SubscriberInitExt, Layer as _};
+
+const SERVER_ADDR: &str = "0.0.0.0:9090";
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let layer = Layer::new().with_filter(LevelFilter::INFO);
+    tracing_subscriber::registry().with(layer).init();
+
+    let listener = TcpListener::bind(SERVER_ADDR).await?;
+    let app = Router::new().route("/", get(|| async move { "Hello, World!" }));
+
+    axum::serve(listener, app).await?;
+    Ok(())
 }
